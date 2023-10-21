554410bce6f07ba202cfc39008c230b5003c2ee9
Michael Auchter <a@phire.org>
2023-10-21 09:42:38 -0500

    fan_control: only run fans automatically when it's warm

--- src/fan_control.py
+++ src/fan_control.py
@@ -5,6 +5,8 @@ import appdaemon.plugins.hass.hassapi as hass
 # - presence_entity: home/away entity.
 # - motion_entity: sensor to indicate motion was detected
 # - timeout: how long after last motion was detected before turning off, in minutes
+# - temperature_entity: temperature sensor for the room
+# - temperature_threshold: threshold below which the fan should be off
 class FanControl(hass.Hass):
     def initialize(self):
         self.log("init FanControl")
@@ -16,6 +18,7 @@ class FanControl(hass.Hass):
         self.motion = False
         self.enable = True
         self.at_home = True
+        self.warm_enough = True
         self.timer = None
         if 'enable' in self.args:
             self.listen_state(self.enable_cb, self.args.get('enable'), immediate=True)
@@ -25,6 +28,10 @@ class FanControl(hass.Hass):
 
         if 'motion_entity' in self.args:
             self.listen_state(self.motion_cb, self.args.get('motion_entity'), new_state='on')
+
+        if 'temperature_entity' in self.args:
+            self.temperature_threshold = float(self.args.get('temperature_threshold', 75))
+            self.listen_state(self.temperature_cb, self.args.get('temperature_entity'), immediate=True)
 
         self.listen_state(self.fan_cb, self.args.get('fan_entity'))
 
@@ -51,6 +58,17 @@ class FanControl(hass.Hass):
         self.log(f'at_home: {self.at_home}')
         self.update_state()
 
+    def temperature_cb(self, entity, attribute, old, new, *kwargs):
+        temp = float(new)
+        if self.warm_enough and temp < (self.temperature_threshold - 1):
+            self.log(f'temp: {temp} fell below {self.temperature_threshold - 1}, disabling fan')
+            self.warm_enough = False
+            self.update_state()
+        elif not self.warm_enough and temp > (self.temperature_threshold + 1):
+            self.log(f'temp: {temp} above {self.temperature_threshold + 1}, enabling fan')
+            self.warm_enough = True
+            self.update_state()
+
     def enable_cb(self, entity, attribute, old, new, *kwargs):
         self.log(f"enable: {new}")
         self.enable = new != "off"
@@ -65,6 +83,9 @@ class FanControl(hass.Hass):
             state = False
         elif self.motion:
             state = True
+
+        if not self.warm_enough:
+            state = False
 
         if state != self.cached_state:
             if state:
